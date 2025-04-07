1018c6336eee483d445dd0720aef5dfc6ef78f26
Enzo <enzomazer@gmail.com>
2025-04-07 18:18:30 -0400

    aula07

--- controllers/veiculoController.js
+++ controllers/veiculoController.js
@@ -1,23 +1,32 @@
 const path = require("path")
+const { PrismaClient } = require('@prisma/client')
+
+const client = new PrismaClient();
+
 
 class veiculoController {
     static formCadastro(req, res) {
         res.sendFile(path.join(__dirname, "../", "views", "formVeiculo.html"));
     }
 
-    static cadastrar(req, res) {
+    static async cadastrar(req, res) {
         const {modelo,placa,ano,cor} = req.body
 
-        const veiculo = client.veiculo.create({data:{
+        const veiculo = await client.veiculo.create({
+            data:{
             modelo,
+            cor,
             placa,
-            ano parseInt(ano),
-            cor
+            ano: parseInt(ano)
+            
         }})
-        res.send(veiculo.id)
+        res.send({veiculo})
     }
 
-    static buscarTodos(req, res) {}
+    static async buscarTodos(req, res) {
+        const veiculos = await client.veiculo.findMany({})
+        res.send(JSON.stringify(veiculos))
+    }
 }
 
 module.exports = veiculoController;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5432
+const port = 3000
 const veiculoRoutes = require("./routes/veiculoRoutes")
 
 app.use(express.urlencoded({extended: true}));
